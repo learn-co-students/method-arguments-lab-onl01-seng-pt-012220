dee1958823ff25c6d6692a0ecabd3f0585a5fc76
Canyon Mathis <canyon.mathis@gmail.com>
2020-01-26 07:28:25 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,16 @@
 #introduction_with_language 
  
-def introduction(name, language)
-  puts "Hi, my name is #{name} and im learning to program in #{language}."
+def introduction(name)
+  puts "Hi, my name is #{name}."
+
 end
 
-introduction(josh, ruby)
+
+introduction("Josh")
+
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language("Dan", "Ember.js")
