2aca530512cc4fdae806763f5364885c3ec91a82
Lakeisha McCree <lmccree215@icloud.com>
2020-01-30 03:02:34 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,5 +2,10 @@ def introduction (name)
   puts "Hi, my name is #{name}."
 end
 
+introduction ("Dan")
+
 def introduction (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction ("Dan", "Ember")
